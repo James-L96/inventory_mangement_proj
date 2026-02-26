bd2c8a730251c10586a1347bcfec366ee74f5298
James Lincoln <jameslincoln1996@gmail.com>
2026-02-25 22:16:10 -0600

    Place TRUCKS and COMPANIES variables to their perspect classes. Start methods work

--- carrier.py
+++ carrier.py
@@ -1,25 +1,29 @@
 class Carrier:
         COMPANIES = ['FedEx', 'UPS', 'RXO', 'XPO']
-        TRUCKS = ['D1', 'D2', 'D3', 
-                'E1', 'E2', 'E3',
-                'F1', 'F2', 'F3',
-                'G1', 'G2', 'G3',]
         def __init__(self,carrier_id,name,company):
             if company not in Carrier.COMPANIES:
                 raise ValueError(f'Invalid carrier company: {company}')
             self.name = name
             self.carrier_id = carrier_id
 
-
+        def assign_truck(self, truck):
+            pass
 class Truck:
+        TRUCKS = ['D1', 'D2', 'D3', 
+                'E1', 'E2', 'E3',
+                'F1', 'F2', 'F3',
+                'G1', 'G2', 'G3',]
+
         def __init__(self,truck_id,capacity, truck):
             if truck not in Carrier.TRUCKS:
                 raise ValueError(f'Invalid truck Id.')
-            
             self.truck_id = truck_id
             self.capacity = capacity
             self.available = True
             self.current_load = 0
+        
+        def dispatch_truck(self):
+            pass
 
 
 
